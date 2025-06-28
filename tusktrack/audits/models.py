2a86a163ceb5b241beae520348180c15c054e1fb
Alrik Schnapke <alrik.schnapke+GitHub@gmail.com>
2025-06-28 12:00:58 +0000

    init Django

---
+++ tusktrack/audits/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Audit(models.Model):
+    title = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    responsible = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=[
+        ('planned', 'Geplant'),
+        ('running', 'Laufend'),
+        ('done', 'Abgeschlossen')
+    ])
+
+    def __str__(self):
+        return self.title
